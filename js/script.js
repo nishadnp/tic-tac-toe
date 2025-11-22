3500d4e7f2a3ac5ffd705aea0f48ff5e630850f8
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-11-22 10:56:19 +0400

    feat: Implement Gameboard module
    Gameboard module with getBoard, setMark, resetBoard

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const Gameboard =  (function() {
+    let gameboard = ['','X','','','','','O','',''];
+    
+    function getBoard() {
+        return gameboard;
+    }
+
+    function resetBoard() {
+        gameboard = ['','','','','','','','',''];
+    }
+
+    function setMark(position, mark) {
+        if (gameboard[position] !== '') 
+        {
+            return;
+        }
+        gameboard[position] = mark;
+    }
+
+    return {
+        getBoard, 
+        resetBoard,
+        setMark,
+    };
+})();
